cb93b6b5796803eb1099254839e88bd412de2b0e
Asiyanaveed <160754902+Asiyanaveed@users.noreply.github.com>
2024-03-01 17:32:25 +0500

    Add files via upload

---
+++ 11th - Copy.js
@@ -0,0 +1,4 @@
+var member = ['Asiya', 'Naveed', 'Warisha', 'Abdul Ahad'];
+for (var i = 0; i < member.length; i++) {
+    console.log(member[i]);
+}
---
+++ 11th.ts
@@ -0,0 +1,4 @@
+let member: string []=['Asiya','Naveed','Warisha','Abdul Ahad'];
+for(let i=0; i<member.length;i++){
+    console.log(member[i]);
+}
